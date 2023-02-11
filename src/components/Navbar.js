affcba33304d3c5c8f368c3c9c438e53cf61ec53
PrimarisEquilibrium <incorrectclock1@gmail.com>
2023-02-11 09:36:51 -0500

    Added Title Removed Icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import "../styles/navbar.css"
 function Navbar({active}) {
   return (
     <nav className="nav">
-      <div className="logo">LOGO</div>
+      <div className="logo">KHOURY DESIGN AND PERMIT DRAWINGS</div>
       <div className="nav-options">
         {active === "home" ? <a href="/" className="nav-btn active">Home</a> : <a href="/" className="nav-btn">Home</a>}
         {active === "contact" ? <a href="/contact" className="nav-btn active">Contact</a> : <a href="/contact" className="nav-btn">Contact</a>}
